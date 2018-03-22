8fe859d22e6bbb417ab9c439c03664e097d6dd44
SpanishJoplin <jpreynoldsku@gmail.com>
2018-03-21 18:20:20 -0600

    Commit 5 - Getting to know you answered

--- answers.js
+++ answers.js
@@ -21,7 +21,7 @@ for (i = 0; i <= 20; i ++) {
 */
 
 //Fizz Buzz
-
+/*
 for (i = 1; i <= 100; i++) {
 	if (i % 3 === 0) {
 		console.log("Fizz");
@@ -36,3 +36,16 @@ for (i = 1; i <= 100; i++) {
 		console.log(i);
 	}
 }
+*/
+
+//Getting to know you
+
+const thom = ["Thom", 1000, "Christchurch"]
+const karolin = ["Karolin", 16, "New York"]
+const kristyn = ["Kristyn", 5, "Pittsburgh"]
+const matt = ["Matt H", 186, "Philadelphia"]
+
+thom[0] = "Gameboy";
+karolin[1] = 17;
+matt[2] = "Gotham City";
+kristyn[2] = "Brooklyn";
