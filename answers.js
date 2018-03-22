fcb70d5fc7f48292a7c2984c3196b82ef4081ab5
SpanishJoplin <jpreynoldsku@gmail.com>
2018-03-21 18:12:33 -0600

    Commit 4 - Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -14,8 +14,25 @@ for (i = 0; i <= 200; i += 2) {
 
 
 //Excited Kitten loop
-
+/*
 for (i = 0; i <= 20; i ++) {
 	console.log("Love me, pet me! HSSSSSS!");
 }
+*/
 
+//Fizz Buzz
+
+for (i = 1; i <= 100; i++) {
+	if (i % 3 === 0) {
+		console.log("Fizz");
+	}
+	if (i % 5 === 0) {
+		console.log("Buzz");
+	}
+	if (i % 3 === 0 && i % 5 === 0) {
+		console.log("FizzBuzz");
+	}
+	else {
+		console.log(i);
+	}
+}
